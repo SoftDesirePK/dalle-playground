8a21a71786497402f1472210614cdf964d07a9c1
SoftDesirePK <freelancetechflame@gmail.com>
2023-07-25 10:29:32 +0300

    Update stable_diffusion_wrapper.py

--- backend/stable_diffusion_wrapper.py
+++ backend/stable_diffusion_wrapper.py
@@ -32,7 +32,7 @@ class StableDiffusionWrapper:
             pipe = pipe.to("cuda")
         
         if repo_id == "dreamlike-art/dreamlike-photoreal-2.0":     # has license
-            pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+            pipe = StableDiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
             pipe = pipe.to("cuda")
 
         
