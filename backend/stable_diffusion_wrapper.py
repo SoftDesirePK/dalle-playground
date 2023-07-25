50119da06c61cc5884835cde52719f70a1107b69
SoftDesirePK <freelancetechflame@gmail.com>
2023-07-25 09:04:44 +0300

    Update stable_diffusion_wrapper.py

--- backend/stable_diffusion_wrapper.py
+++ backend/stable_diffusion_wrapper.py
@@ -5,13 +5,20 @@ class StableDiffusionWrapper:
     def __init__(self) -> None:
         repo_id = "stabilityai/stable-diffusion-2-base"
         repo_id = "stabilityai/stable-diffusion-2-1"
-        #repo_id = "stabilityai/stable-diffusion-xl-base-0.9"
+        repo_id = "stabilityai/stable-diffusion-xl-base-0.9"
 
-        pipe = DiffusionPipeline.from_pretrained(
-            repo_id, revision="fp16",
-            torch_dtype=torch.float16
-        )
+        if repo_id == "stabilityai/stable-diffusion-2-base":
+            pipe = DiffusionPipeline.from_pretrained(
+                repo_id, revision="fp16",
+                torch_dtype=torch.float16
+            )
 
+
+        if repo_id == "stabilityai/stable-diffusion-xl-base-0.9":
+            pipe = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+            pipe.to("cuda")
+            
+        
         pipe.scheduler = DPMSolverMultistepScheduler.from_config(
             pipe.scheduler.config)
         self.pipe = pipe.to("cuda")
